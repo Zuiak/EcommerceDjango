a27a95fda9e099556bd7e61d3f8d4b2ff50e123a
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-14 22:45:49 +0200

    Add Category and Product models

--- EcommerceDjango/urls.py
+++ EcommerceDjango/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('blog/', include('blog.urls'), name='blog'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=15, unique=True)
+    description = models.TextField(blank=True)
+    img = models.ImageField(upload_to='category', blank=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=30, unique=True)
+    img = models.ImageField(upload_to='products', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('updated', )
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
