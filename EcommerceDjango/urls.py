a0cbff4f093c6bd8b4678df87e1a34f69d65585b
Herman Zuiakov <gzuyakov@gmail.com>
2022-07-15 22:08:25 +0200

    Products on home page

--- EcommerceDjango/urls.py
+++ EcommerceDjango/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('blog/', include('blog.urls'), name='blog'),
+    path('<slug:category_slug>', views.home, name='products_by_category'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=30, unique=True)
-    img = models.ImageField(upload_to='products', blank=True)
+    image = models.ImageField(upload_to='products', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
